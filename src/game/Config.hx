6b15dc204cae235d45ef2782c4dfc3d1d8df69c9
Gauthier Billot <gogoprog@gmail.com>
2019-11-10 21:34:47 +0100

    Functional displacement

--- src/game/Config.hx
+++ src/game/Config.hx
@@ -6,4 +6,5 @@ class Config {
     public static var tileSize:Float = 8;
     public static var halfTileSize:Float = tileSize / 2;
     public static var gridOffset = new Vector2(tileSize/2, tileSize/2);
+    public static var playerRadius:Float = 0.15;
 }
--- src/game/PlayerSystem.hx
+++ src/game/PlayerSystem.hx
@@ -65,14 +65,20 @@ class PlayerSystem extends ListIteratingSystem<PlayerNode> {
                 var worldDir:Vector3 = Vector3.TransformNormal(d, localMat);
                 worldDir.y = 0;
                 worldDir.normalize();
-                var requestedPosition = t.position + worldDir * 5 * dt;
-                tryMove(node, requestedPosition);
+                // var requestedPosition = t.position + worldDir * 5 * dt;
+                var speed:Float = 5;
+
+                if(keys["Shift"]) {
+                    speed = 0.5;
+                }
+
+                tryMove(node, worldDir * speed * dt);
             }
         }
         {
             var move = whiplash.Input.mouseMove;
-            player.pitch += move.y * 0.01;
-            player.yaw += move.x * 0.01;
+            player.pitch += move.y * 0.001;
+            player.yaw += move.x * 0.001;
 
             t.setRotationFromYawPitchRoll(player.yaw, player.pitch, 0);
         }
@@ -96,27 +102,70 @@ class PlayerSystem extends ListIteratingSystem<PlayerNode> {
         new js.jquery.JQuery("canvas")[0].requestPointerLock();
     }
 
-    private function tryMove(node:PlayerNode, position:Vector3) {
-        var currentPosition = node.transform.position;
+    private function tryMove(node:PlayerNode, displacement:Vector3) {
         var map = Game.instance.currentMap;
         var walls = map.walls;
         var offset = new Vector2(map.width, map.height) * -0.5;
-        var playerLine = new phaser.geom.Line(currentPosition.x - offset.x, currentPosition.z - offset.y, position.x - offset.x, position.z - offset.y);
-        var playerCircle = new phaser.geom.Circle(position.x - offset.x, position.z - offset.y, 0.3);
+        var currentPos = node.transform.position;
+        var playerCurrentMapPos = new Vector2(currentPos.x - offset.x, currentPos.z - offset.y);
+        var playerMapPos:Vector2 = null;
+        var mapDisplacement = new Vector2(displacement.x, displacement.z);
+        var playerEndMapPos = playerCurrentMapPos + mapDisplacement;
         var collides = false;
+        var collidingWall:game.map.Wall = null;
+        var nearest = new Vector2();
+        var count = 1;
 
-        for(wall in walls) {
-            // var r = untyped Phaser.Geom.Intersects.LineToLine(playerLine, wall);
-            var r = untyped Phaser.Geom.Intersects.LineToCircle(wall, playerCircle);
+        for(i in 0...count) {
+            var f = (i+1)/count;
+            playerMapPos = playerCurrentMapPos + mapDisplacement * f;
+            var playerCircle = new phaser.geom.Circle(playerMapPos.x, playerMapPos.y, Config.playerRadius);
 
-            if(r) {
-                collides = true;
+            for(wall in walls) {
+                var r = untyped Phaser.Geom.Intersects.LineToCircle(wall, playerCircle, nearest);
+
+                if(r) {
+                    collidingWall = wall;
+                    collides = true;
+                    break;
+                }
+            }
+
+            if(collides) {
                 break;
             }
         }
 
         if(!collides) {
-            node.transform.position = position;
+            node.transform.position += displacement;
+        } else {
+            var normal:Vector2 = playerMapPos - nearest;
+            normal.normalize();
+            playerMapPos = playerEndMapPos;
+            var i = 0;
+
+            while(collides) {
+                collides = false;
+                playerMapPos += normal * 0.01;
+                var playerCircle = new phaser.geom.Circle(playerMapPos.x, playerMapPos.y, Config.playerRadius);
+
+                for(wall in walls) {
+                    var r = untyped Phaser.Geom.Intersects.LineToCircle(wall, playerCircle);
+
+                    if(r) {
+                        collides = true;
+                        break;
+                    }
+                }
+
+                ++i;
+
+                if(i > 100) {
+                    return;
+                }
+            }
+
+            node.transform.position.set(playerMapPos.x + offset.x, 0, playerMapPos.y + offset.y);
         }
     }
 }
